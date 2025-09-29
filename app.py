72ff5e98eda0356108f0ff833c8f718f1e84fa43
Vikas Pathak <vikaspathak0911@gmail.com>
2025-09-29 11:22:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -78,4 +78,4 @@ def health():
 
 # This block is for local development and will not be used by Vercel.
 if __name__ == "__main__":
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
